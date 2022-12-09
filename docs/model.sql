68f8e6dd7b223cdf5294c506c83d267df9d6decc
fang <lovefyj616@foxmail.com>
2022-12-10 02:57:51 +0800

    feat: 初始数据更新

--- docs/model.sql
+++ docs/model.sql
@@ -1,67 +1,62 @@
 create database if not exists `recruit-system` ;
 use `recruit-system`;
--- person 表
-create table if not exists `person`
-(
-    `id` int not null auto_increment comment 'id' primary key ,
-    `username` varchar(256) not null comment '用户名',
-    `password` varchar(256) null comment '密码',
-    `name` varchar(64) null comment '姓名',
-    `gender` int null comment '性别',
-    `birth_date` datetime not null comment '出生日期',
-    `school` varchar(256) null comment '学校',
-    `degree` varchar(256) null comment '学历',
-    `telephone` varchar(24) not null comment '联系电话',
-    `email` varchar(45) default 'NULL' null comment '邮箱'
-) comment '求职者表';
 
--- HR 表
-create table if not exists `hr`
-(
-    `id` int not null auto_increment comment 'id' primary key ,
-    `username` varchar(256) not null comment '用户名',
-    `password` varchar(256) null comment '密码',
-    `name` varchar(64) null comment '姓名',
-    `gender` int null comment '性别(0-女, 1-男)',
-    `company` varchar(256) comment '公司',
-    `telephone` varchar(24) not null comment '联系电话',
-    `email` varchar(45) default 'NULL' null comment '邮箱'
-) comment '求职者表';
+CREATE TABLE `bio_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `person_id` int(11) NOT NULL COMMENT '用户ID',
+  `url` varchar(1024) NOT NULL COMMENT '简历pdf地址',
+  `job_id` int(11) NOT NULL COMMENT '岗位ID',
+  `status` int(11) DEFAULT '0' COMMENT '状态(0-未处理, 1-已处理)',
+  `interview_content` text COMMENT '面试内容',
+  `interview_title` varchar(256) DEFAULT NULL COMMENT '面试邮件',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8  COMMENT='投递记录表';
 
--- job 表
-create table if not exists `job`
-(
-`id` int not null auto_increment comment 'id' primary key,
-`name` varchar(64) null comment '岗位名称',
-`desc` text null comment '岗位描述',
-`city` varchar(64) null comment '所在城市',
-`salary_low` float null comment '最低工资，单位K',
-`salary_high` float null comment '最高工资，单位K',
-`is_done` int null comment '是否在招' DEFAULT 1,
-`degree` int null comment '学历要求',
-`experience` int null comment '工作经验要求',
-`hr_id` int null comment 'HR id'
-) comment '工作表';
+CREATE TABLE `hr` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` varchar(256) NOT NULL COMMENT '用户名',
+  `password` varchar(256) DEFAULT NULL COMMENT '密码',
+  `name` varchar(64) DEFAULT NULL COMMENT '姓名',
+  `gender` int(11) DEFAULT NULL COMMENT '性别',
+  `company` varchar(256) DEFAULT NULL COMMENT '公司',
+  `telephone` varchar(24) NOT NULL COMMENT '联系电话',
+  `email` varchar(45) DEFAULT 'NULL' COMMENT '邮箱',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='求职者表';
 
--- star表
-create table if not exists `star`
-(
-`id` int not null auto_increment comment 'id' primary key,
-`person_id` int not null comment '用户ID',
-`job_id` int not null comment '岗位ID'
-) comment '岗位收藏夹表';
+CREATE TABLE `job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(64) DEFAULT NULL COMMENT '岗位名称',
+  `desc` text COMMENT '岗位描述',
+  `city` varchar(64) DEFAULT NULL COMMENT '所在城市',
+  `salary_low` float DEFAULT NULL COMMENT '最低工资，单位K',
+  `salary_high` float DEFAULT NULL COMMENT '最高工资，单位K',
+  `is_done` int(11) DEFAULT '1' COMMENT '是否在招',
+  `degree` int(11) DEFAULT NULL COMMENT '学历要求',
+  `experience` int(11) DEFAULT NULL COMMENT '工作经验要求',
+  `hr_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8 COMMENT='岗位表';
 
--- bio_record 表
-create table if not exists `bio_record`
-(
-    `id` int not null auto_increment comment 'id' primary key,
-    `person_id` int not null comment '用户ID',
-    `url` varchar(1024) not null comment '简历pdf地址',
-    `job_id` int not null comment '岗位ID',
-    `status` int null comment '状态(0-未处理, 1-已处理)' DEFAULT 0,
-    `interview_content` text null comment '面试内容',
-    `interview_title` varchar(256) null comment '面试邮件',
-    `create_time` datetime not null comment '创建时间' default CURRENT_TIMESTAMP,
-    `update_time` datetime not null comment '更新时间' default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
-);
+CREATE TABLE `person` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` varchar(256) NOT NULL COMMENT '用户名',
+  `password` varchar(256) DEFAULT NULL COMMENT '密码',
+  `name` varchar(64) DEFAULT NULL COMMENT '姓名',
+  `gender` int(11) DEFAULT NULL COMMENT '性别',
+  `school` varchar(256) DEFAULT NULL COMMENT '学校',
+  `degree` varchar(256) DEFAULT NULL COMMENT '学历',
+  `telephone` varchar(24) NOT NULL COMMENT '联系电话',
+  `email` varchar(45) DEFAULT 'NULL' COMMENT '邮箱',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='求职者表';
+
+CREATE TABLE `star` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `person_id` int(11) NOT NULL COMMENT '用户ID',
+  `job_id` int(11) NOT NULL COMMENT '岗位ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8 COMMENT='岗位收藏夹表';
 
